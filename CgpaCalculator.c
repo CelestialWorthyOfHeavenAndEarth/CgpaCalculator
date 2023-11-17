81096abba584f3c941756e3c91f72b005ea15844
CelestialWorthyOfHeavenAndEarth <150166569+CelestialWorthyOfHeavenAndEarth@users.noreply.github.com>
2023-11-17 22:46:11 +0530

    Update CgpaCalculator.c

--- CgpaCalculator.c
+++ CgpaCalculator.c
@@ -8,7 +8,8 @@
 
 /*
     File Name  :CgpaCalculator.c
-    purpose    :To find the overall cgpa of students.
+    purpose    :The primary purpose of the CGPA Calculator project is to provide students with an accessible,  user-friendly, and accurate tool for determining their
+                CGPA. It addresses the challenges associated with manual CGPA calculations, offering a digital solution that simplifies the process and ensures correctness. 
     Author name:Y.Aswini kumar
                 B.Lokesh praman
     created by  :Y.Aswini kumar
