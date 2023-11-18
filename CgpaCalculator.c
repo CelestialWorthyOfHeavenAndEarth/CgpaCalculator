c1a6117b6d55b8758d043c6aa883e8929338b972
CelestialWorthyOfHeavenAndEarth <150166569+CelestialWorthyOfHeavenAndEarth@users.noreply.github.com>
2023-11-18 21:01:25 +0530

    Update CgpaCalculator.c

--- CgpaCalculator.c
+++ CgpaCalculator.c
@@ -17,25 +17,37 @@
     updated on  :17/11/2023
 */
 
+// SOURCE CODE:
+                                             // CGPA CALCULATOR
+
+
 #include <stdio.h>
 #include <string.h>
 
 int main() 
 {
+    printf("          -----------------\n");
+    printf("          |CGPA CALCULATOR|  \n");
+    printf("          -----------------\n");
+    // Variable to store the total number of subjects
     int totalSubjects;
     printf("Enter the total number of subjects: ");
     scanf("%d", &totalSubjects);
 
+    // Variables to store total credits and total grade points
     float totalCredits = 0.0;
     float totalGradePoints = 0.0;
 
     // Arrays to store subject names, credits, and grades
+
     char subjectNames[totalSubjects][50];
     float credits[totalSubjects];
     float grades[totalSubjects];
 
     // Input subject details for each subject
-    for (int i = 0; i < totalSubjects; i++) {
+
+    for (int i = 0; i < totalSubjects; i++)
+    {
         printf("Enter the name for subject %d: ", i + 1);
         scanf("%s", subjectNames[i]);
 
@@ -45,19 +57,30 @@ int main()
         printf("Enter the grade for subject %d: ", i + 1);
         scanf("%f", &grades[i]);
 
+        // Update total credits and total grade points
         totalCredits += credits[i];
         totalGradePoints += credits[i] * grades[i];
     }
 
+    // Calculate CGPA
     float cgpa = totalGradePoints / totalCredits;
 
-    printf("Your CGPA is: %.2f\n", cgpa);
+    // Display CGPA
+    printf("----------------------");
+
+    printf("\n| Your CGPA is: %.2f |\n", cgpa);
+
+    printf("----------------------");
 
     // Display the subject names and corresponding credits and grades
-    printf("Subject Details:\n");
-    for (int i = 0; i < totalSubjects; i++) {
+    printf("\n");
+    printf("\nSubject Details:\n");
+    for (int i = 0; i < totalSubjects; i++)
+    {
         printf("Subject: %s, Credits: %.1f, Grade: %.1f\n", subjectNames[i], credits[i], grades[i]);
     }
 
     return 0;
 }
+
+
